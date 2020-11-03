ba4e48cb1fa79bb0ca5089132d5912ce8221efb2
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-11-03 21:45:38 +0100

    Add Swagger

--- homework3/src/main/java/com/springcourse/homework3/Homework3Application.java
+++ homework3/src/main/java/com/springcourse/homework3/Homework3Application.java
@@ -3,6 +3,7 @@ package com.springcourse.homework3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Homework3Application {
 
