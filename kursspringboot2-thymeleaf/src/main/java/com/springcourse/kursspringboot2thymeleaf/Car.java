84fc84cb97e52e7fe3becbd6f9276de816235b81
marcinPluciennik <marcinpluciennik76@gmail.com>
2020-11-10 19:02:59 +0100

    Add element

--- kursspringboot2-thymeleaf/src/main/java/com/springcourse/kursspringboot2thymeleaf/Car.java
+++ kursspringboot2-thymeleaf/src/main/java/com/springcourse/kursspringboot2thymeleaf/Car.java
@@ -28,4 +28,12 @@ public class Car {
     public void setMark(String mark) {
         this.mark = mark;
     }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "mark='" + mark + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
 }
